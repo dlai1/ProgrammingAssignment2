004402b2c65ecfb7e27806052c983953b715fb47
dl <48105252+dlai1@users.noreply.github.com>
2019-03-28 21:25:06 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Assignment 2 Coursera Solutions
 
-## Generates invertible matrix, has a cached inverse that can be stored later
+## Generates invertible matrix, has an object m where the inverse can be stored later
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -10,23 +10,23 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         
 	  get <- function() x
-        
+        	
 	  setinv <- function(inverse) m <<- inverse
-        getinv <- function() m
-        list(set = set, 
-		 get = get,
-             setinv = setinv,
-             getinv = getinv)
+          getinv <- function() m
+          list(set = set, 
+	       get = get,
+	       setinv = setinv,
+	       getinv = getinv)
 }
 
-
-## Solves and inverts the matrix, inverted matrix is stored and returned as m
+## Solves and inverts the matrix, inverted matrix is stored and returned as object m
 
 cacheSolve <- function(x, ...) {
         m <- x$getinv()
-        if(!is.null(m)) {
+        ## If a matrix is already cached, it is returned without solving again
+	if(!is.null(m)) {
                 message("getting cached data")
-                return(m)
+                return(m) 
         }
         data <- x$get()
         m <- solve(data)
